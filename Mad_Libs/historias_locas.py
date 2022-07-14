6bc165192ac3c28327b259e8e1838665a2852195
Alan Paz  (AIPA) <israel.pazar@gmail.com>
2022-07-14 14:57:37 -0500

    Proyecto Mad_Libs

---
+++ Mad_Libs/historias_locas.py
@@ -0,0 +1,10 @@
+organizacion= "freeCodeCamp"
+print("Aprende a programar con "+ organizacion)
+adj = input("Ingrese un adjetivo: ")
+verbo1 = input("Verbo:  ")
+verbo2 = input("Verbo: ")
+sustantivo_plural = input("Sustantivo (plural): ")
+
+madlib = f"programar es tan {adj}! Siempre me emocina porque me encanta {verbo1} problemas. ¡Aprende a {verbo2} con FreCodeCamp y alacanza tus {sustantivo_plural}!"
+
+print(madlib)
